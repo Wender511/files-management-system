76f6d796eeebceba98c5f4ebc44b591f812668c3
NguyenDinhHoang479 <hoangnd479@gmail.com>
2023-10-28 12:09:06 +0700

    update routes

---
+++ src/components/layout/DefaultLayout/index.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+const DefaultLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+}
+
+export default DefaultLayout;
---
+++ src/page/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return ( <>This is page home</> );
+}
+ 
+export default Home;
