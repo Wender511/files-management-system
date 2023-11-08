c98059649a544061fcfc57f803ee66afe08c2419
NguyenDinhHoang479 <hoangnd479@gmail.com>
2023-11-08 14:26:01 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,50 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import { authRoutes } from './Routes';
-import { DefaultLayout } from './components/layout';
 import LoginPage from './pages/Login';
+import { LoginLayout } from './components/layout';
+
 
 interface rootReducer {
   auth: boolean;
 }
 function App() {
   const loginInfo: any = useSelector((state: rootReducer) => state.auth);
-  console.log(loginInfo);
-
+  console.log("this is a",loginInfo.userData.user);
+  
   return (
-    <>
-      <Router>
-        <Routes>
-          {authRoutes.map((route, index) => {
-            let Layout;
-            let Page = LoginPage;
-            if (loginInfo.user) {
-              Page = route.component;
-            }
-
-            if (route.layout) {
-              Layout = route.layout;
-            } else {
-              Layout = Fragment;
-            }
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <Layout>
-                    <Page />
-                  </Layout>
-                }
-              ></Route>
-            );
-          })}
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        {authRoutes.map((route, index) => {
+          let Layout;
+          let Page = LoginPage;
+          if (loginInfo.userData.data) {
+            Page = route.component;
+          } else if (loginInfo.userData.user == null){
+            return <Route key={index}  path={route.path} element={ <LoginLayout><Page /></LoginLayout> }
+          />
+          }
+          if (route.layout) {
+            Layout = route.layout;
+          } else {
+            Layout = Fragment;
+          }
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <Layout>
+                  <Page />
+                </Layout>
+              }
+            />
+          );
+        })}
+      </Routes>
+    </Router>
   );
 }
 
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -4,12 +4,12 @@ import MorePage from '../pages/More';
 import LoginPage from '../pages/Login';
 import EmpolyeesPage from '../pages/Empolyees';
 // Layout
-import { DefaultLayout } from '../components/layout';
+import { DefaultLayout, LoginLayout } from '../components/layout';
 
 const authRoutes = [
-  { path: '/', component: Home },
+  { path: '/', component: Home, layout: DefaultLayout },
   { path: '/more', component: MorePage, layout: DefaultLayout },
-  { path: '/login', component: LoginPage },
+  { path: '/login', component: LoginPage, layout: LoginLayout },
   { path: '/employees', component: EmpolyeesPage, layout: DefaultLayout },
 ];
 
