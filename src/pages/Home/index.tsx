e68839c9960eaa11e3eee26ffda09aa9b14f2749
NguyenDinhHoang479 <hoangnd479@gmail.com>
2023-11-16 07:40:18 +0700

    update page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Filelist from "./Filelist";
+import useFile, {fileApi} from "src/api/fileApi";
+interface rootReducer {
+  auth: boolean;
+}
+
 const Home = () => {
-  return (
-    
-    <>
-      <h2 className='text-red-500'>This is page home</h2>
-    </>
-  );
+  // 0 là nhân viên,  1 là duyệt
+  const [role, setRole] = useState(0)
+  const checkrole: any = useSelector((state: rootReducer) => state.auth);
+  const {data: fileData, mutate, error} = useFile();
+
+  if(fileData && fileData.data.data){
+    return <Filelist data = {fileData.data.data} mutate={mutate}/>
+  }
+return(<></>)
 };
 
 export default Home;
